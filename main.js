ab881a946a79209b9c6439993d2cb26ccb472004
maduma <maduma@pt.lu>
2023-01-29 00:34:55 +0100

    rack equipements

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const dcrack = require('./modules/dcrack.js');
 dcrack.createRacks(document, 'rack');
+
+const rackData = [
+    {"name": "t52ffe01", "size_ru": 3, "position": 40},
+    {"name": "bro01-01", "size_ru": 1, "position": 39},
+    {"name": "bro01-02", "size_ru": 1, "position": 38},
+    {"name": "s72hrs01", "size_ru": 2, "position": 36},
+    {"name": "v5200-1", "size_ru": 1, "position": 35},
+]
+
+//validate data with rack size
+//insert equipement in rack

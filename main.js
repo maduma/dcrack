ddaf1646ba9e14a41420e7bee76ebe270bf5c97f
Stéphane Nsakala <stephane.nsakala@hopitauxschuman.lu>
2023-02-10 13:44:25 +0100

    update localdata

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
 const dcrack = require('./modules/dcrack.js');
 
-/**
- * @param {Array} data
- */
-function justStack(data) {
-    let start = 0;
-    data.forEach(d => {
-        d.position = start
-        start += d.size_ru;
-    });
-}
-
-async function asyncDataHttp(rackId) {
-    const response = await fetch(`https://itops.int.hs.lu/inventory/rack/${rackId}/json/`);
-    // const response = await fetch('data.json');
+async function asyncLocalDataHttp(rackId) {
+    const response = await fetch('data.json');
     const data = await response.json();
-    data.sort((a, b) => a.name.localeCompare(b.name));
-    justStack(data)
-    return data;
+    return data[rackId];
 }
 
 async function noData(rackId) {
     return []
 }
 
-dcrack.createRacks(document, 'rack', asyncDataHttp);
+async function asyncRemoteDataHttp(rackId) {
+    const response = await fetch(`https://itops.int.hs.lu/inventory/rack/${rackId}/json/`);
+    const data = await response.json();
+    return data;
+}
+
+dcrack.createRacks(document, 'rack', asyncLocalDataHttp);
 
 const loadButtons = document.querySelectorAll('button.load');
 loadButtons.forEach(button => {
     const rackId = button.getAttribute('rack-id');
     button.addEventListener('click', ev => {
-        dcrack.updateRack(document, rackId, asyncDataHttp);
+        dcrack.updateRack(document, rackId, asyncLocalDataHttp);
     })
 })
